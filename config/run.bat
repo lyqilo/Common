11c490dc314adb085b60f003acb0bec3f37791d6
marfu <manfy1993@163.com>
2023-03-20 14:53:07 +0800

    导表工具修改

--- config/run.bat
+++ config/run.bat
@@ -3,6 +3,6 @@
 del /s /Q .\cpp\
 del /s /Q .\cs\
 del /s /Q .\rof\
-@xlsx2rof.exe ./xlsx/ ./rof/ ./cs/ ./cpp/
+@xlsx2rof.exe ./xlsx/Common/ ./xlsx/Server/ ./rof/Client/ ./rof/Server/ ./cs/ ./cpp/
 @echo SUCCESS!
 @pause
